9891d329b7c4acd432879dcfcd632c56f6fb662d
marmol89 <38738460+marmol89@users.noreply.github.com>
2026-06-25 17:21:10 +0200

    Initial commit: Spring Boot 3.4 + Kotlin project skeleton

---
+++ src/main/kotlin/com/bitacora_x/resource_manager_back/ResourceManagerBackApplication.kt
@@ -0,0 +1,11 @@
+package com.bitacora_x.resource_manager_back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResourceManagerBackApplication
+
+fun main(args: Array<String>) {
+	runApplication<ResourceManagerBackApplication>(*args)
+}
---
+++ src/test/kotlin/com/bitacora_x/resource_manager_back/ResourceManagerBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bitacora_x.resource_manager_back
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ResourceManagerBackApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
