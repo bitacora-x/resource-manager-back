cade265d7ac98b6599acf9429c6c0b475545b924
sikay <pedrohdezmora@gmail.com>
2026-06-25 18:20:00 +0200

    added tans for pre-commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,3 +37,11 @@ kotlin {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+task("addPreCommitGitHookOnBuild") {
+    println("⚈ ⚈ ⚈ Running Add Pre Commit Git Hook Script on Build ⚈ ⚈ ⚈")
+    exec {
+        commandLine("cp", "./.scripts/pre-commit", "./.git/hooks")
+    }
+    println("✅ Added Pre Commit Git Hook Script.")
+}
